d1f0e881a36c669ddeaa2ff91d1556eadfcac9e4
Abhinav <abhinavtiwari2705@gmail.com>
2023-04-02 01:24:27 +0530

    Models created

--- yogshala/Main/models.py
+++ yogshala/Main/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Disease(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+
+class Pain(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    level = models.IntegerField()
+    # disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class User(User):
+    # name = models.CharField(max_length=100)
+    # email = models.EmailField()
+    # password = models.CharField(max_length=100)
+    # phone = models.CharField(max_length=100)
+    Disease=models.ManyToManyField(Disease)
+    Pain=models.ManyToManyField(Pain)
+
+    def __str__(self):
+        return self.name
+
+
+
+class yoga(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='yoga/images/')
+    video = models.URLField()
+    Disease=models.ManyToManyField(Disease)
+    Pain=models.ManyToManyField(Pain)
+
+    def __str__(self):
+        return self.name
+
+
+
+
--- yogshala/Main/urls.py
+++ yogshala/Main/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.urls import path,include
 from Main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
